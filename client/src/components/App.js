4f431ecddbb4c6d7994141398bdb1b239f92faba
Gabe <gabriel.gelgor@ontariotechu.net>
2021-06-08 13:56:04 -0400

    Client fixes

---
+++ client/src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+const App = () => {
+    return(
+        
+        <div>
+            <BrowserRouter>
+                <div>
+                    App
+                </div>
+            </BrowserRouter>
+        </div>
+    );
+};
+
+export default App;
